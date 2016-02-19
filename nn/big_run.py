7c3a86ac11f4a60e2a81312c4b50fe9264b11ae4
sam6321 <sam6321@live.com.au>
2016-02-19 13:50:49 +0800

    Removing un needed things

--- nn/big_run.py
+++
@@ -1,6 +0,0 @@
-from network_pybrain import run_network
-from network import run_network_keras
-# Try a really big run.
-print 'Starting big run...'
-run_network_keras(100, 30, 'mse', None, input_filter_types=None)
-print 'Done'
